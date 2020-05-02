d0b31fddfa75ed5770d630a9ef9bf654d04a762b
Kirill Liutikov <kirill.liutikov@gmail.com>
2020-05-02 22:00:24 +0300

    implements interface

---
+++ src/ru/basejava/storage/Storage.java
@@ -0,0 +1,20 @@
+package ru.basejava.storage;
+
+import ru.basejava.model.Resume;
+
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+}
