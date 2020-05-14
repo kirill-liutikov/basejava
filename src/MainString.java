d1af823a1474a6a260edc458e8af1e6d5609c4a7
Kirill Liutikov <kirill.liutikov@gmail.com>
2020-05-14 15:56:52 +0300

    Lesson4 Reflection and JUnit

--- src/MainString.java
+++ src/MainString.java
@@ -1,6 +1,7 @@
 
 
 
+
 public class MainString {
     public static void main(String[] args) {
         String[] strArray = new String[]{"1", "2", "3", "4", "5"};
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -26,7 +26,7 @@ public class MainTestArrayStorage {
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+//        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
         printAll();
 
         r1.setName("John");
